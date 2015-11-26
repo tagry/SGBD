3bdedd7501e1bd6a92b99e64439f5014fcece14f
tagry <tagry@ipb.fr>
2015-11-26 16:02:26 +0100

    ok

--- src/menu.java
+++ src/menu.java
@@ -1,24 +1,137 @@
 import javax.swing.JOptionPane;
 
 public class  menu{
-	public static void main(String[] args) {
-		int continuer = 1;
-		String[] choixPossible = {"Info utilisateur", "Rechercher Recette", "Moyenne des prix des recette d'un éleve","Nombre de recette dispo pour une categorie","Recette trié par meilleur rapport qualité prix","Deserts les plus rapide à réaliser","Trié par plats plus commentés", "écart type le plus faible", "ajout de recette","suppression de recette","suppression d'un aliment", "suppression d'un éleve"};
-		JOptionPane jop = new JOptionPane(), jop2 = new JOptionPane();
-		JOptionPane jop3 = new JOptionPane();            
-
-
-
-		while(continuer==1)
+	public static void main(String[] args)
 		{
-			String nom = (String)jop.showInputDialog(null,"Que voulez vous faire ?","Choix requete",JOptionPane.QUESTION_MESSAGE,null, choixPossible,choixPossible[0]);
-			int choixRetenu = transformNomInt(choixPossible, nom);
-			jop2.showMessageDialog(null, "Votre choix est " + choixRetenu, "Etat civil", JOptionPane.INFORMATION_MESSAGE);
-
-			continuer = jop3.showConfirmDialog(null, "Voulez-vous arrêter l'animation ?", "Arrêt de l'animation", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
-			
+			choixPrincipal();			
 		}
-	}
+	
+	private static void choixPrincipal()
+		{
+			int continuer = 1;
+			int choixRetenu = -1;
+			
+			String[] choixPossible = {"Info utilisateur", "Rechercher Recette", "Moyenne des prix des recette d'un éleve","Nombre de recette dispo pour une categorie","Recette trié par meilleur rapport qualité prix","Deserts les plus rapide à réaliser","Trié par plats plus commentés", "écart type le plus faible", "ajout de recette","suppression de recette","suppression d'un aliment", "suppression d'un éleve"};
+			
+			while(continuer==1)
+			{		    
+				choixRetenu = choixOuvertRetourInt(choixPossible, "Que voulez vous faire ?", "Choix requete");
+				transisionChoixSecondaire(choixRetenu);
+				continuer = choixFerme("Voulez vous fermer l'application ?","Fermer l'application");
+				
+			}
+		}
+
+	private static void transisionChoixSecondaire(int choixRetenu)
+		{
+			switch(choixRetenu)
+			{
+			case 0 :
+				infoUtilisateur();
+				break;
+				/*
+			case 1:
+				rechercherRecette();
+				break;
+
+			case 2:
+				moyennePrixEleve();
+				break;
+
+			case 3:
+				nombreRecetteParCategorie();
+				break;
+
+			case 4:
+				classerRecetteQP();//QR qualité prix
+				break;
+
+			case 5:
+				classerDessertRapide();
+				break;
+
+			case 6:
+				classerPlatsPlusC();//C Commentés
+				break;
+
+			case 7:
+				eleveEcartTypeMin();
+				break;
+
+			case 8:
+				ajoutRecette();
+				break;
+
+			case 9:
+				ajoutEleve();
+				break;
+
+			case 10:
+				ajoutAliment();
+				break;
+
+			case 11:
+				supprimerRecette();
+				break;
+				
+			case 12:
+				supprimerEleve();
+				break;
+
+			case 13:
+				supprimerAliment();
+				break;*/
+			}
+		}
+
+	static void infoUtilisateur()
+		{
+			//	requete r;
+			String[] choix = {"Nom", "Numero","Les deux"};
+			int choixRetenu = choixMultipleBouton(choix, "Comment identifier l'utilisateur ?","Utilisateur");
+
+			switch(choixRetenu)
+			{
+			case 0:
+				//	appelerRequete("Utilisateur", "Mot de passe", "Requete1");
+				break;
+
+			case 1:
+				//appelerRequete("Utilisateur", "Mot de passe", "Requete2");
+				break;
+
+			case 2:
+				//appelerRequete("Utilisateur", "Mot de passe", "Requete3");
+				break;
+			}
+
+		}
+
+	private static int choixMultipleBouton(String[] choix, String question, String nomFenetre)
+		{
+			JOptionPane jop = new JOptionPane();
+			return jop.showOptionDialog(null,question,nomFenetre,JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null,choix, choix[0]);
+		}
+	
+	private static int choixOuvertRetourInt(String[] choix,String question, String nomFenetre)
+		{
+			JOptionPane jop = new JOptionPane();
+			String nom = (String)jop.showInputDialog(null,question,nomFenetre,JOptionPane.QUESTION_MESSAGE,null, choix,choix[0]);
+			return transformNomInt(choix, nom);			
+		}
+
+		private static String choixOuvertRetourString(String[] choix,String question, String nomFenetre)
+		{
+			JOptionPane jop = new JOptionPane();
+			return (String)jop.showInputDialog(null,question,nomFenetre,JOptionPane.QUESTION_MESSAGE,null, choix,choix[0]);		
+		}
+
+	private static int choixFerme(String question, String nomFenetre)
+		{
+			JOptionPane jop = new JOptionPane();
+			return jop.showConfirmDialog(null, question, nomFenetre, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);			
+		}
+	
 	private static int transformNomInt(String[] tableau, String nom)
 		{
 			int i = 0;
