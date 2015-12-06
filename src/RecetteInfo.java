d1976fae4b0fec0ee48c2d10aa99ffbd047fcac6
tagry <tagry@ipb.fr>
2015-12-06 14:19:44 +0100

    ok

---
+++ src/RecetteInfo.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class RecetteInfo{
+
+	//donné
+	private String numeroRecette;
+
+	//Resultat: [...]
+
+	public	RecetteInfo(Object numeroRecette)
+		{
+		    this.numeroRecette = (String)numeroRecette;
+			requete();
+		}
+
+	public void requete()
+		{
+			System.out.println("\nRequete vers Recette effectué");
+		}
+
+}
