b99ded4932c8fb1e8f6b34773e3cf7f1b57238f3
SOULAIMANA Wardi <wsoulaimana@mandrill.pedago.ipb.fr>
2015-12-07 08:58:00 +0100

    ajout all.sql qui cree et remplit la BDD

--- src/script.bash
+++ src/script.bash
@@ -1,7 +1,20 @@
-echo "Script de creation de la base"
+#!/bin/bash
+###############################################################
+# 
+# Fichier de configuration d'Oracle
+#
+###############################################################
+# A lancer une fois connecté sur la machine Oracle  
+# -------------------------------------------------------------
 
-ssh oracle
-echo "definition de CLASSPATH"
+echo "Configuration d'Oracle ... "
+
+export PATH=$PATH:/home/oracle/app/oracle/product/11.2.0/dbhome_1/bin
+export ORACLE_HOME=/home/oracle/app/oracle/product/11.2.0/dbhome_1
+export ORACLE_SID=oracle
+export ORACLE_HOME_LISTNER=$ORACLE_HOME
 export CLASSPATH=$ORACLE_HOME/jdbc/lib/ojdbc5.jar:.:$CLASSPATH
 
-sqlplus wsoulaimana wsoulaimana
+echo $CLASSPATH
+
+echo "Configuration ok ! "
